d7570c1b964901d9a562382b0be1f2352f4dc42a
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2021-12-28 10:24:53 +0530

    Conceptual changes in  of Armstrong number

--- Loops-Implementation/program14.c
+++ Loops-Implementation/program14.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
+
 
 int main() {
     printf("Enter upper limit: ");
@@ -10,9 +10,14 @@ int main() {
 
     int i = 1;
     while (i <= n) {
+        int j = i, digits = 0;
+        do {
+            digits++;
+            j /= 10;
+        } while(j > 0);
         int sum = 0;
-        for (int j = i; j > 0; j /= 10)
-            sum += pow((double)(j % 10), 3.0);
+        for (j = i; j > 0; j /= 10)
+            sum += pow((double)(j % 10), digits);
         if (sum == i) printf("%d\n", i);
         i++;
     }
