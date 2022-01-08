a94055b82c2d4ab4c0b1cf4fa4b1b2fb22e667dc
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2022-01-08 21:44:14 +0530

    tweaks

--- Arrays-Implementation/Questions/prog24.c
+++ Arrays-Implementation/Questions/prog24.c
@@ -1,17 +1,6 @@
 // Author: Kunal Kumar Sahoo
 // Objective: Write a program in C to find the sum of left diagonals of a matrix. 
-// Test Data :
-// Input the size of the square matrix : 2
-// Input elements in the first matrix :
-// element - [0],[0] : 1
-// element - [0],[1] : 2
-// element - [1],[0] : 3
-// element - [1],[1] : 4
-// Expected Output :
-// The matrix is :
-// 1 2
-// 3 4
-// Addition of the left Diagonal elements is :5
+
 
 #include <stdio.h>
 
