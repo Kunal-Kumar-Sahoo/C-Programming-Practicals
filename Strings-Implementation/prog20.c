19a29fed93782d63b15f1f8227e28e1f50ea9602
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2022-01-15 13:27:18 +0530

    patch-1

--- Strings-Implementation/prog20.c
+++ Strings-Implementation/prog20.c
@@ -1,10 +1,5 @@
-// 20. Write a program in C to find the largest and smallest word in a string. 
-// Test Data :
-// Input the string : It is a string with smallest and largest word.
-// Expected Output :
-// The largest word is 'smallest'
-// and the smallest word is 'a' 
-// in the string : 'It is a string with smallest and largest word.'.
+// Author: Kunal Kumar Sahoo
+// Objective: Write a program in C to find the largest and smallest word in a string. 
 
 #include <stdio.h>
 #include <string.h>
